625599ea97fac2f8d214db542e168ce98f6886c3
Chris Davenport <chris@christopherdavenport.tech>
2019-09-09 18:59:04 -0700

    Add Baseline for Teams Endpoint I want

---
+++ core/src/main/scala/io/chrisdavenport/github/endpoints/organizations/Teams.scala
@@ -0,0 +1,30 @@
+package io.chrisdavenport.github.endpoints.organizations
+
+import cats.implicits._
+import cats.effect._
+
+import org.http4s._
+import org.http4s.implicits._
+
+import io.chrisdavenport.github.Auth
+import io.chrisdavenport.github.internals.GithubMedia._
+import io.chrisdavenport.github.internals.RequestConstructor
+
+import io.chrisdavenport.github.data.Teams._
+
+
+object Teams {
+
+  def addOrUpdateTeamRepo[F[_]: Sync](
+    teamId: Int,
+    organization: String,
+    repoName: String,
+    permission: Permission,
+    auth: Auth
+  ) = RequestConstructor.runRequestWithBody[F, AddTeamRepoPermission, Unit](
+    auth.some,
+    Method.PUT,
+    uri"/teams" / teamId.toString / "repos" / organization / repoName,
+    AddTeamRepoPermission(permission)
+  )
+}
